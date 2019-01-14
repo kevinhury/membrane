bb92ee5f558fc40cdc18689b73bced00c16b9f2c
Kevin Hury <kevinh306@gmail.com>
2019-01-14 21:07:04 +0200

    request query plugin

--- reverseproxy/hooks/reqtransform/reqtransform.go
+++ reverseproxy/hooks/reqtransform/reqtransform.go
@@ -29,7 +29,10 @@ func (h Hook) PreHook(r *http.Request, w http.ResponseWriter, plugin config.Plug
 		}
 	}
 	if action.Query != nil {
-		return nil
+		err := modifyRequestQuery(r, w, plugin)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -38,31 +41,50 @@ func (h Hook) PreHook(r *http.Request, w http.ResponseWriter, plugin config.Plug
 func modifyRequestBody(r *http.Request, w http.ResponseWriter, plugin config.Plugin) error {
 	action := plugin.Action.(actions.RequestTransform)
 
-	if action.Body.Duplicate != nil {
-		b, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			return err
-		}
-		bodyFormatted := make(map[string]interface{})
-		err = json.Unmarshal(b, &bodyFormatted)
-		if err != nil {
-			return err
-		}
-
-		for key, newKey := range action.Body.Duplicate {
-			if value, found := bodyFormatted[key]; found {
-				bodyFormatted[newKey] = value
-			}
-		}
-
-		b, err = json.Marshal(bodyFormatted)
-		if err != nil {
-			return err
-		}
-
-		r.ContentLength = int64(len(b))
-		r.Body = ioutil.NopCloser(bytes.NewBuffer(b))
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
 	}
+	bodyFormatted := make(map[string]interface{})
+	err = json.Unmarshal(b, &bodyFormatted)
+	if err != nil {
+		return err
+	}
+
+	for key, newKey := range action.Body.Duplicate {
+		if value, found := bodyFormatted[key]; found {
+			bodyFormatted[newKey] = value
+		}
+	}
+
+	for key, val := range action.Body.Append {
+		bodyFormatted[key] = val
+	}
+
+	b, err = json.Marshal(bodyFormatted)
+	if err != nil {
+		return err
+	}
+
+	r.ContentLength = int64(len(b))
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(b))
+
+	return nil
+}
+
+func modifyRequestQuery(r *http.Request, w http.ResponseWriter, plugin config.Plugin) error {
+	action := plugin.Action.(actions.RequestTransform)
+
+	q := r.URL.Query()
+	for key, newKey := range action.Query.Duplicate {
+		q.Add(newKey, q.Get(key))
+	}
+
+	for key, val := range action.Query.Append {
+		q.Add(key, val)
+	}
+
+	r.URL.RawQuery = q.Encode()
 
 	return nil
 }
